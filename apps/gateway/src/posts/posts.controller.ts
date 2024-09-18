8858255175446ff18698d8b010e09004a4e34910
MihPar <9496260@gmail.com>
2024-09-18 20:39:43 +0300

    feat: to delete end points

--- apps/gateway/src/posts/posts.controller.ts
+++ apps/gateway/src/posts/posts.controller.ts
@@ -12,16 +12,6 @@ export class PostsController {
     return this.postsService.create(createPostDto);
   }
 
-  @Get()
-  findAll() {
-    return this.postsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.update(+id, updatePostDto);
