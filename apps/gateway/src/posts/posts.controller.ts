fc7c506f995bea06337396ed34095a2898ddda2f
MihPar <9496260@gmail.com>
2024-09-22 20:29:11 +0300

    feat: uncommit of end point @Patch update()

--- apps/gateway/src/posts/posts.controller.ts
+++ apps/gateway/src/posts/posts.controller.ts
@@ -12,10 +12,10 @@ export class PostsController {
     return this.postsService.create(createPostDto);
   }
 
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-//     return this.postsService.update(+id, updatePostDto);
-//   }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
+  }
 
 //   @Delete(':id')
 //   remove(@Param('id') id: string) {
